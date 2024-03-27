1a26e40732b123f2c044bee94eab22daa62e3a07
pete <pietro.dellabriottaparolo@gmail.com>
2024-03-27 14:11:54 +0200

    changes

---
+++ utils.py
@@ -0,0 +1,20 @@
+import os,logging
+
+def file_exists(fname):
+    '''
+    Function to pass to type in argparse
+    '''
+    if os.path.isfile(fname):
+        return str(fname)
+    else:
+        print(fname + ' does not exist')
+        sys.exit(1)
+ 
+log_levels = {
+    'critical': logging.CRITICAL,
+    'error': logging.ERROR,
+    'warn': logging.WARNING,
+    'warning': logging.WARNING,
+    'info': logging.INFO,
+    'debug': logging.DEBUG
+}
