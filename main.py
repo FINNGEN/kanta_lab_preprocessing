961e74dee7d8344b05b5d72a57da240a255ba578
pete <pietro.dellabriottaparolo@gmail.com>
2024-04-02 13:25:01 +0300

    working example with multiprocessing

--- main.py
+++ main.py
@@ -22,38 +22,63 @@ def chunk_reader(raw_file,chunk_size):
 
 
 def all_filters(df,args):
-    b = True if "Puuttuu" in df else False
-    if b:print(f"before:{df.laboratoriotutkimusnimikeid}")
-    df.pipe(filter_minimal,args)
-    if b:print(f"after:{df.laboratoriotutkimusnimikeid}")
+    df.pipe(filter_minimal,args).pipe(filter_minimal,args).pipe(filter_minimal,args).pipe(filter_minimal,args)
     return df
 
 
 def main(args):
-
     logger.info(f"Input path:{args.raw_data}")
     out_file = os.path.join(args.out,f"{args.prefix}_munged.txt")
-    pool = mp.Pool(args.jobs)
+   
     # get function with only df as input
     multi_func = partial(all_filters,args=args)
     # read in chunk and apply further split
+    logger.info("START")
     for i,chunk in enumerate(chunk_reader(args.raw_data,args.chunk_size)):
-        df_split = np.array_split(chunk, args.jobs)
-        chunksize = len(df_split[0])
-        results = pool.imap(multi_func, df_split,chunksize=chunksize)
-        df = pd.concat(list(results))
+        df= all_filters(chunk,args)
+        # write header for first chunk along with df and print some info
         if i ==0:
             logger.debug(f"before:{chunk.laboratoriotutkimusnimikeid}")
             logger.debug(f"after:{df.laboratoriotutkimusnimikeid}")
             size = len(df)
             df.to_csv(out_file, mode='w', index=False, header=True,sep="\t")
             logger.info(f"chunksize:{len(df)}")
-            logger.info(f"sub chunk size:{chunksize}")
         else:
             size += len(df)
             df.to_csv(out_file, mode='a', index=False, header=False,sep="\t")
         progressBar(str(size))
     print('\nDone.')
+    logger.info("END")
+    return
+
+def multi_main(args):
+
+    logger.info(f"Input path:{args.raw_data}")
+    out_file = os.path.join(args.out,f"{args.prefix}_munged.txt")
+    ctx = mp.get_context('spawn')
+    pool = ctx.Pool(args.jobs)
+    # get function with only df as input
+    multi_func = partial(all_filters,args=args)
+    # read in chunk and apply further split
+    logger.info("START")
+    for i,chunk in enumerate(chunk_reader(args.raw_data,args.chunk_size)):
+        df_split = np.array_split(chunk, args.jobs)
+        results = pool.imap(multi_func, df_split)
+        df = pd.concat(list(results))
+        # write header for first chunk along with df and print some info
+        if i ==0:
+            logger.debug(f"before:{chunk.laboratoriotutkimusnimikeid}")
+            logger.debug(f"after:{df.laboratoriotutkimusnimikeid}")
+            size = len(df)
+            df.to_csv(out_file, mode='w', index=False, header=True,sep="\t")
+            logger.info(f"chunksize:{len(df)}")
+            logger.info(f"sub chunk size:{len(df_split)}")
+        else:
+            size += len(df)
+            df.to_csv(out_file, mode='a', index=False, header=False,sep="\t")
+        progressBar(str(size))
+    print('\nDone.')
+    logger.info("END")
     return
 
 
@@ -64,6 +89,8 @@ if __name__=='__main__':
     parser.add_argument("--raw-data", type=file_exists, help =  "Path to input raw file", default = os.path.join(dir_path,"test","raw_data_test.txt"))
     parser.add_argument("--log",  default="warning", choices = log_levels, help=(  "Provide logging level. Example --log debug', default='warning'"))
     parser.add_argument("--test",action='store_true')
+    parser.add_argument("--mp",action='store_true',help="run multiproc")
+
     parser.add_argument("--jobs",default = os.cpu_count(),type = int, help ="number of jobs to run in parallel")
     parser.add_argument('-o',"--out",type = str, help = "Folder in which to save the results", default = os.getcwd())
     parser.add_argument("--prefix",type=str,default="kanta")
@@ -82,5 +109,9 @@ if __name__=='__main__':
 
     # make sure the chunk size is at least the size of the the jobs
     args.chunk_size = max(args.chunk_size,args.jobs)
-    main(args)
+    if mp:
+        multi_main(args)
+    else:
+        main(args)
+        
     logger.debug("END")
